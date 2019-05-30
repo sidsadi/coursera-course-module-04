835394e4d6b119d19e2cb15a2786f818a52fb614
sidsadi <51020606+sidsadi@users.noreply.github.com>
2019-05-30 12:06:33 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var names = [Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
+for var (i=0; i< name.length;i++){
+ var firstletter = names[i].CharAt(0).toLowerCase();
+ if (firstletter === 'j'){
+ GoodBye.jSomeName(names[i]);
+ else{
+ hello SomeName(names[i]);
