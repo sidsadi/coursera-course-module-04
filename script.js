339d17d156f3e70911c44ac8196dd5d828bbec5e
sidsadi <51020606+sidsadi@users.noreply.github.com>
2019-05-31 11:26:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-var names = [Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
+var names = [yasir", "Jawad", "John", "Javeed", "Parihan", "Farukh", "Larib", "Piya", "Laiba", "Jahnjaeeb"]
 for var (i=0; i< name.length;i++){
  var firstletter = names[i].CharAt(0).toLowerCase();
  if (firstletter === 'j'){
  GoodBye.jSomeName(names[i]);
  else{
  hello SomeName(names[i]);
+ }
+ }  
+    })();
+
+(function (window){
+    var jSomeName = {};
+    var speakWord = "Good Bye";
+    
+    jsomeName.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.jSomeName = SomeName;
+    })(window);
